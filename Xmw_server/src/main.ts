6830b9d0da36b3cb154d98afc37909971fb96ad6
huangzhiwei4 <huangzhiwei4@yy.com>
2024-07-05 22:29:43 +0800

    feat: 调整配置

--- Xmw_server/src/main.ts
+++ Xmw_server/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   // 启动cors跨域
   app.enableCors({
     credentials: true,
-    origin: ['http://116.196.112.102', 'http://localhost:8888'],
+    origin: ['http://106.53.56.87', 'http://localhost:8888'],
   });
 
   // 配置 session
